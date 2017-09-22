948e518306ae53ca4e14f1b8e1e915251f936978
stephanos <stephanos@goldmail.etsu.edu>
2017-09-22 09:40:15 -0700

    Complete

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -12,12 +12,26 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var lightBulb: UIImageView!
     
+    @IBAction func lightSwitch(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0:
+                lightBulb.backgroundColor = UIColor.red
+            case 1:
+                lightBulb.backgroundColor = UIColor.yellow
+            case 2:
+                lightBulb.backgroundColor = UIColor.blue
+            case 3:
+                lightBulb.backgroundColor = UIColor.green
+            default:
+                lightBulb.backgroundColor = UIColor.black
+        }
+    }
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        lightBulb.backgroundColor = UIColor.blue
+        lightBulb.backgroundColor = UIColor.red
         
     }
     
